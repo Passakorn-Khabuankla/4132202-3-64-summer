4d4618e2971ab3b4f7abe43c576d10e0aca2c051
Passakorn-Khabuankl <630112418017@bru.ac.th>
2022-04-22 11:58:54 +0700

    lab4

---
+++ lab4/www/php/profile.php
@@ -0,0 +1,10 @@
+<?php
+
+echo "Passakorn Khubankla";
+echo "<br>";
+echo "ID : 630112418017";
+echo "<br>";
+echo "Information Technology";
+echo "<br>";
+echo "Buriram Rajabhat University";
+echo "<br>";
